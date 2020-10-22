0ec22cc878fc920fdaa87dce2d19deb2b4599f9d
Thai Do <thai.hl.do@gmail.com>
2020-10-21 23:35:53 -0700

    Still trying to fix authentication routes for front end

--- src/services/auth/index.js
+++ src/services/auth/index.js
@@ -9,34 +9,46 @@ export default {
 // v->s->|->c->m->db
 const data = {}
 
-function login() {
-  let request = {
-    email: document.getElementById('emailInput').value,
-    password: document.getElementById('passwordInput').value,
-  }
+function login(email, password) {
+  // const requestOptions = {
+  //   method: 'POST',
+  //   headers: { 'Content-Type': 'application/json' },
+  //   body: JSON.stringify({ email, password })
+  // };
 
-  fetch('/', {
-    method: "POST",
-    body: "MY DATA",
-    headers: {
-      "Content-Type": "applicationa/json"
-    }
-  })
-  .then(function(response) {
-    return response.json();
-  })
-  .then(function(myJson) {
-    console.log(myJson);
-  })
+  // return fetch('/manager', requestOptions)
+  //   .then(user => {
+  //     localStorage.setItem('user', JSON.stringify(user))
+
+  //     return user
+  //   })
   // ping backend with the username/password
   // it should return true/false or a token
   // if is authenticated
-  // history.push('/manager')
+  history.push('/manager')
   // else
   // redirect to failed login page
 }
 
 function logout() {
   // destroy cookie/token
+  localStorage.removeItem('user');
   history.push('/')
+}
+
+function handleResponse(response) {
+  return response.text().then(text => {
+      const data = text && JSON.parse(text);
+      if (!response.ok) {
+          if (response.status === 401) {
+              // auto logout if 401 response returned from api
+              logout();
+          }
+
+          const error = (data && data.message) || response.statusText;
+          return Promise.reject(error);
+      }
+
+      return data;
+  });
 }
