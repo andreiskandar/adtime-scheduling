54285640a384724b3e219e4c04c3601c0b068a0a
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-10-23 19:26:02 -0700

    worked on ui-shifts

--- src/components/Schedule/components/EmployeeGrid.js
+++ src/components/Schedule/components/EmployeeGrid.js
@@ -1,27 +1,37 @@
 import React from 'react';
 import './employeeGrid.scss';
+import ShiftBlock from './ShiftBlock';
+import axios from 'axios';
 
 const EmployeeGrid = () => {
-  let array = [];
-  array.length = 91;
-  const renderSpan = Array.from({ length: 91 }, (x, i) => {
-    return <span data-id={i} />;
-  });
-  // const renderSpan = array.map((i, idx) => {
-  //   console.log(object)
-  //   if (idx === 2) {
-  //     return <div>render block</div>;
-  //   } else {
-  //     return <span data-id={idx} />;
-  //   }
-  // });
+  const userShifts = [
+    { id: 1, user_id: 1, shift_id: 1, ispublished: false, category_id: 1, date: '2020-10-23T10:00:00.000Z' },
+    { id: 2, user_id: 1, shift_id: 2, ispublished: false, category_id: 1, date: '2020-10-23T15:00:00.000Z' },
 
-  return (
-    <div className='employee_grid'>
-      {renderSpan}
-      {/* {Array( 7 * 13 ).fill(<span />)} */}
-    </div>
-  );
+    { id: 3, user_id: 1, shift_id: 3, ispublished: false, category_id: 1, date: '2020-10-25T19:00:00.000Z' },
+    { id: 4, user_id: 1, shift_id: 4, ispublished: false, category_id: 1, date: '2020-10-25T21:00:00.000Z' },
+
+    { id: 5, user_id: 1, shift_id: 5, ispublished: false, category_id: 1, date: '2020-10-26T12:00:00.000Z' },
+    { id: 6, user_id: 1, shift_id: 6, ispublished: false, category_id: 1, date: '2020-10-26T09:00:00.000Z' },
+  ];
+
+  // 10 - 15 = 6 hours
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    console.log(e.target);
+  };
+
+  const renderSpan = Array.from({ length: 91 }, (x, i) => {
+    if (i > 6 && i < 11) {
+      return <span data-id={i} className='color' onClick={handleClick} />;
+    } else {
+      return <span data-id={i} onClick={handleClick} />;
+    }
+  });
+
+  return <div className='employee_grid'>{renderSpan}</div>;
 };
 
 export default EmployeeGrid;
