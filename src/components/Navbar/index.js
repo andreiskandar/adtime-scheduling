7e4de8d3da5358802a499cd055c7f926e77e7ab8
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-10-24 12:57:30 -0700

    before merge to master

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,25 +1,31 @@
-import React from 'react'
-import Typography from '@material-ui/core/Typography'
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
 import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
-import VpnKeyOutlinedIcon from '@material-ui/icons/VpnKeyOutlined';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
-import './styles.scss'
+import './styles.scss';
 
 export default (props) => {
   // this will receive props username then to show in the navbar
-  // if user is logged in, hide login link 
+  // if user is logged in, hide login link
   return (
-    <div className="navbar">
-      <div className="navbar links__navbar">
+    <div className='navbar'>
+      <div className='navbar links__navbar'>
         <Typography variant='h4'>
-          <a href='#' src="" className="navbar logo__navbar"><img src="" alt="" className="logo_img__navbar"></img>A.D.Time</a>
+          <a href='#' src='' className='navbar logo__navbar'>
+            <img src='' alt='' className='logo_img__navbar'></img>A.D.Time
+          </a>
         </Typography>
-        <div className="navbar right_links__navbar">
-          <a href='#' src="" className="navbar link__navbar"><AccountCircleOutlinedIcon className="icon icon__navbar"/>username</a>
-          <a href='#' src="" className="navbar link__navbar"><LockOutlinedIcon className="icon icon__navbar"/>Logout</a>
+        <div className='navbar right_links__navbar'>
+          <a href='#' src='' className='navbar link__navbar'>
+            <AccountCircleOutlinedIcon className='icon icon__navbar' />
+            username
+          </a>
+          <a href='#' src='' className='navbar link__navbar'>
+            <LockOutlinedIcon className='icon icon__navbar' />
+            Logout
+          </a>
         </div>
       </div>
     </div>
-    
-  )
-}
+  );
+};
