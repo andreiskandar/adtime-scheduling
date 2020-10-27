1be0a64935dce1116a20a1f239e421ac3e01623e
davidRamalho <david.m.ramalho@gmail.com>
2020-10-26 19:27:01 -0600

    isolated style problem to 'antd/dist/antd.css'

--- src/components/WeekNav/MiniCalendar/MiniCalendar.js
+++ src/components/WeekNav/MiniCalendar/MiniCalendar.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import 'antd/dist/antd.css';
 import './MiniCalendar.scss';
 import { Calendar } from 'antd';
+//import 'antd/dist/antd.css';
 
 
 const MiniCalendar = () => {
--- src/components/WeekNav/index.js
+++ src/components/WeekNav/index.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
+import './WeekCalendar.scss';
 import WeekCalendar from './WeekCalendar';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 import { Dialog, DialogActions, DialogTitle, Button, TextField, Avatar } from '@material-ui/core';
-//import MiniCalendar from './MiniCalendar/MiniCalendar'
-import './WeekCalendar.scss';
+import MiniCalendar from './MiniCalendar/MiniCalendar'
+
 
 export default () => {
   const [open, setOpen] = useState(false);
@@ -26,7 +27,7 @@ export default () => {
       <Button onClick={openCalendar}>
         <WeekCalendar />
         <Dialog open={open} onClose={handleClose} maxWidth='lg'>
-         
+         <MiniCalendar></MiniCalendar>
         </Dialog>
       </Button>
 
