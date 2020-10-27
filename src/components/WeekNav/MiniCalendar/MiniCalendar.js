5da67b7750df6ab89aa896d7d4ed8c9a294e3e00
davidRamalho <david.m.ramalho@gmail.com>
2020-10-27 01:16:59 -0600

    working on calendar

--- src/components/WeekNav/MiniCalendar/MiniCalendar.js
+++ src/components/WeekNav/MiniCalendar/MiniCalendar.js
@@ -2,6 +2,9 @@ import React from 'react';
 import './MiniCalendar.css';
 import './index.css';
 import { Calendar } from 'antd';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
 
 const MiniCalendar = () => {
   
@@ -9,11 +12,16 @@ const MiniCalendar = () => {
     console.log(value, mode);
   }
   
+  const handleChange = (e) => {
+    console.log(e.currentTarget)
+  }
+
   return (
     <div className="site-calendar-demo-card">
-      <Calendar fullscreen={false} onPanelChange={onPanelChange} />
+      <Calendar onSelect = {handleChange} fullscreen={false} onPanelChange={onPanelChange} />
     </div>
   );
 };
 
 export default MiniCalendar;
+
