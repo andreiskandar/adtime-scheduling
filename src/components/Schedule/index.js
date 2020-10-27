2bf59d7bd68f2f1e0ca8dae5b9d77e7d72a5aa8a
Thai Do <thai.hl.do@gmail.com>
2020-10-26 20:53:34 -0700

    Trying to set up render for transfer shift

--- src/components/Schedule/index.js
+++ src/components/Schedule/index.js
@@ -19,8 +19,6 @@ export default () => {
   const [shift, setShift] = useState([]);
 
   useEffect(() => {
-    transferShift();
-    cancelShift();
     publishSchedule();
     const apiUsers = axios.get('/api/users');
     const apiUserShift = axios.get('api/shifts/events');
@@ -63,6 +61,18 @@ export default () => {
     })
   }  
 
+  const transferShiftId = (user_id, shift_id, start_time, end_time, category_id, transferToId) => {
+    let payload = transferShift(user_id, shift_id, start_time, end_time, category_id, transferToId)
+    axios.put('/api/events/transfer', payload)
+      .then(() => {
+        console.log("Good")
+      })
+      .catch((e) => {
+        console.log("Error from transfering shift(s)", e);
+      });
+   };
+  
+
   const employees = users.map((user) => {
     return (
       <Employee
@@ -71,7 +81,7 @@ export default () => {
         users={users}
         submitShift={submitShift}
         removeShift={removeShift}
-        transferShift={transferShift}
+        transferShiftId={transferShiftId}
         shift={shift}
         setShift={setShift}
       />
--- src/helpers/transferShift.js
+++ src/helpers/transferShift.js
@@ -1,5 +1,15 @@
-const transferShift = () => {
-  console.log('hELLOW');
+const transferShift = (user_id,  start_time, end_time, transferToId) => {
+  const shift_id = [];
+  const startTime = parseInt(start_time);
+  const endTime = parseInt(end_time);
+  //hardcoded category_id until functionality is added
+  const category_id = 1;
+
+  for (let i = (startTime); i <= (endTime); i++) {
+    shift_id.push(i - 8);
+  }
+
+  return {user_id, shift_id, start_time, end_time, category_id, transferToId};
 };
 
 export default transferShift;
