841c070d8128fbe1edf731c23f8006ebbe219043
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-10-27 23:38:05 -0700

    before merge fixed on transfer Shift

--- src/components/Schedule/index.js
+++ src/components/Schedule/index.js
@@ -71,7 +71,7 @@ export default (props) => {
   };
 
   const transferShiftId = (user_id, shift_id, start_time, end_time, transferToUserId, event_date, category_id) => {
-    let payload = transferShift(user_id, shift_id, start_time, end_time, transferToUserId, event_date, category_id);
+    let payload = transferShift(user_id, start_time, end_time, transferToUserId, event_date, category_id);
     axios
       .put('/api/events/transfer', payload)
       .then(() => {
