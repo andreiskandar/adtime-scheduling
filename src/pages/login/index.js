0f22f54fa28ab58e49ec58b242333a516a1da40f
Thai Do <thai.hl.do@gmail.com>
2020-11-02 16:22:25 -0800

    Removed more console logs to clean up code

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -72,7 +72,7 @@ export default () => {
               }}
             />
             <TextField
-              type='text  '
+              type='password'
               label='Password'
               for='passwordInput'
               id='passwordInput'
