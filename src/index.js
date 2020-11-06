cb7e06b042d81976b574bae5b653cf32676cce56
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-11-06 15:58:57 -0800

    added axios

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import axios from 'axios';
 
 if (process.env.REACT_APP_API_BASE_URL) {
   axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
