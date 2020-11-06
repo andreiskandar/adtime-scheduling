f6111a5324a3607e92dfd9fb43fc1c1865e8e62c
Andre Iskandar <andre.m.iskandar@gmail.com>
2020-11-06 15:54:33 -0800

    added REACT_APP_API_BASE_URL

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+if (process.env.REACT_APP_API_BASE_URL) {
+  axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
